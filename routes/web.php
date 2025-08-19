076d2498253b321fdeaafaa5e68c741ab179bc85
zosarillana <zhou991818@gmail.com>
2025-08-19 10:25:48 +0800

    added cors for api config to front

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// API routes
+Route::prefix('api')->group(function () {
+    Route::get('/test', function () {
+        return response()->json(['message' => 'Hello from Laravel 12 API!']);
+    });
+});
